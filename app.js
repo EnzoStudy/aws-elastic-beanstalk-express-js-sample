04f9e1ad7caf00c7135f66dafd814b4aa66705a0
jaeho shin (Enzo) <35325262+EnzoStudy@users.noreply.github.com>
2024-09-02 01:32:58 +0900

    기본 파일

--- app.js
+++ app.js
@@ -1,31 +1,7 @@
-const express = require('express');
 const app = express();
-
-// 포트 번호 설정
 const port = 8080;
 
-app.get('/', (req, res) => {
-    // 현재 날짜와 시간 객체 생성
-    const now = new Date();
 
-    // 시, 분, 초 추출
-    const hours = now.getHours();
-    const minutes = now.getMinutes();
-    const seconds = now.getSeconds();
+app.get('/', (req, res) => res.send('Hello World!'));
 
-    // 두 자리 수 포맷팅 (예: 01, 09 등)
-    const formattedHours = hours.toString().padStart(2, '0');
-    const formattedMinutes = minutes.toString().padStart(2, '0');
-    const formattedSeconds = seconds.toString().padStart(2, '0');
-
-    // 현재 시간 문자열 생성
-    const currentTime = `${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
-
-    // 'Hello World!'와 현재 시간 함께 출력
-    res.send(`Hello World! The current time is: ${currentTime}`);
-});
-
-// 서버 시작
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-});
+app.listen(port);
