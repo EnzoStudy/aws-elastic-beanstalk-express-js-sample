74fb2a5afcddd2f1267f442118773711040ad0ca
jaeho shin (Enzo) <35325262+EnzoStudy@users.noreply.github.com>
2024-09-01 18:12:42 +0900

    file upload

--- app.js
+++ app.js
@@ -2,6 +2,26 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
+
+// 현재 날짜와 시간 객체 생성
+const now = new Date();
+
+// 시, 분, 초, 밀리초 추출
+const hours = now.getHours();
+const minutes = now.getMinutes();
+const seconds = now.getSeconds();
+
+// 두 자리 수 포맷팅 (예: 01, 09 등)
+const formattedHours = hours.toString().padStart(2, '0');
+const formattedMinutes = minutes.toString().padStart(2, '0');
+const formattedSeconds = seconds.toString().padStart(2, '0');
+
+// 현재 시간 문자열로 출력
+const currentTime = `${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
+console.log("현재 시간:", currentTime);
+
+
+
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(port);
